044a9a9a63e8e18d48ee4f76603d08ab30059a04
Sanich <sancho@cdrpro.ru>
2019-01-02 22:38:17 +0700

    Updates

--- precache-manifest.273e59ce487d476d0abf17aa084464f6.js
+++ precache-manifest.273e59ce487d476d0abf17aa084464f6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-family-tree-example/static/js/runtime~main.6679984b.js"
   },
   {
-    "revision": "19a4759792088cd45578",
-    "url": "/react-family-tree-example/static/js/main.19a47597.chunk.js"
+    "revision": "f5769675b45b588f486c",
+    "url": "/react-family-tree-example/static/js/main.f5769675.chunk.js"
   },
   {
     "revision": "b888ac39e5fe039c1270",
     "url": "/react-family-tree-example/static/js/1.b888ac39.chunk.js"
   },
   {
-    "revision": "19a4759792088cd45578",
+    "revision": "f5769675b45b588f486c",
     "url": "/react-family-tree-example/static/css/main.fee1be51.chunk.css"
   },
   {
-    "revision": "40c8861f57748b5cd6a678fa1aa960a6",
+    "revision": "44d3c01423af657422c560a7ed871dbb",
     "url": "/react-family-tree-example/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-family-tree-example/precache-manifest.289b7f3c1cf5bd6e9501e80d5213fea5.js"
+  "/react-family-tree-example/precache-manifest.273e59ce487d476d0abf17aa084464f6.js"
 );
 
 workbox.clientsClaim();
