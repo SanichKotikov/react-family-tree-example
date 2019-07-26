1a5ccad5dfdfa5c4cb452ca700848c7e018707b0
Sanich <sancho@cdrpro.ru>
2019-07-26 23:40:28 +0300

    Updates

---
+++ precache-manifest.dbc505900e204db9ca858b4bd26ee562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0888319b405d53a72b6f7dab7c0a52db",
+    "url": "/react-family-tree-example/index.html"
+  },
+  {
+    "revision": "cfb3e750a640f911cf5d",
+    "url": "/react-family-tree-example/static/css/main.af935d8c.chunk.css"
+  },
+  {
+    "revision": "649782d53ecd47565e0e",
+    "url": "/react-family-tree-example/static/js/2.267ed325.chunk.js"
+  },
+  {
+    "revision": "cfb3e750a640f911cf5d",
+    "url": "/react-family-tree-example/static/js/main.b93fb8e7.chunk.js"
+  },
+  {
+    "revision": "895d916a98f6c5e3ecc3",
+    "url": "/react-family-tree-example/static/js/runtime~main.25f0ace7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-family-tree-example/precache-manifest.2212cc30efcbf2969dee07be9b352a8c.js"
+  "/react-family-tree-example/precache-manifest.dbc505900e204db9ca858b4bd26ee562.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-family-tree-example/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-family-tree-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
